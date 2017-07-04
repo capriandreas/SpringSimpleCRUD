445301ecc9341678bf301b348ca3321f53084b51
Andreas Capri <capriandreas@gmail.com>
2017-07-04 13:53:24 +0700

    Spring Boot JPA Configuration

---
+++ src/main/java/com/example/demo/model/Mahasiswa.java
@@ -0,0 +1,52 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Mahasiswa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	
+	@Column(name = "nama_mahasiswa")
+	private String nama;
+	
+	private String jurusan;
+	
+	private String nim;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getJurusan() {
+		return jurusan;
+	}
+
+	public void setJurusan(String jurusan) {
+		this.jurusan = jurusan;
+	}
+
+	public String getNim() {
+		return nim;
+	}
+
+	public void setNim(String nim) {
+		this.nim = nim;
+	}
+	
+}
